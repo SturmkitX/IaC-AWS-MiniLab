3dca0aeeb36defb49be360bffb94fe31b36be863
Bogdan Rogoz <bogdanius96@gmail.com>
2023-05-09 16:58:23 +0300

    added security groups and assigned network interfaces to instances

--- terraform-kube/main.tf
+++ terraform-kube/main.tf
@@ -31,11 +31,47 @@ resource "aws_subnet" "kube-subnet-01" {
   }
 }
 
+resource "aws_security_group" "kube-master-sg" {
+  name    = "kube-master-sg-01"
+  vpc_id  = aws_vpc.kube-vpc-01.id
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "Ping"
+    protocol         = "icmp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_network_interface" "kuber-master-01-nic" {
+  subnet_id = aws_subnet.kube-subnet-01.id
+  security_groups = [aws_security_group.kube-master-sg.id]
+}
 
 resource "aws_instance" "kube-master-01" {
   ami           = "ami-064087b8d355e9051"
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.kube-subnet-01.id
+
+  network_interface {
+    network_interface_id  = aws_network_interface.kuber-master-01-nic.id
+    device_index          = 0
+  }
+  
   key_name      = aws_key_pair.kube-ssh-keypair-01.key_name
 
   tags = {
--- terraform-kube/workers.tf
+++ terraform-kube/workers.tf
@@ -1,7 +1,49 @@
+resource "aws_security_group" "kube-worker-sg" {
+  name    = "kube-worker-sg-01"
+  vpc_id  = aws_vpc.kube-vpc-01.id
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "Ping"
+    protocol         = "icmp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_network_interface" "kuber-worker-01-nic" {
+  subnet_id = aws_subnet.kube-subnet-01.id
+  security_groups = [aws_security_group.kube-worker-sg.id]
+}
+
+resource "aws_network_interface" "kuber-worker-02-nic" {
+  subnet_id = aws_subnet.kube-subnet-01.id
+  security_groups = [aws_security_group.kube-worker-sg.id]
+}
+
 resource "aws_instance" "kube-worker-01" {
   ami           = "ami-064087b8d355e9051"
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.kube-subnet-01.id
+
+  network_interface {
+    network_interface_id  = aws_network_interface.kuber-worker-01-nic.id
+    device_index          = 0
+  }
+
   key_name      = aws_key_pair.kube-ssh-keypair-01.key_name
 
   tags = {
@@ -14,6 +56,12 @@ resource "aws_instance" "kube-worker-02" {
   ami           = "ami-064087b8d355e9051"
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.kube-subnet-01.id
+
+  network_interface {
+    network_interface_id  = aws_network_interface.kuber-worker-02-nic.id
+    device_index          = 0
+  }
+
   key_name      = aws_key_pair.kube-ssh-keypair-01.key_name
 
   tags = {
