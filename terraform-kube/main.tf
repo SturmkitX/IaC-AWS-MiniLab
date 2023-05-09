0ebcb01c94cde139d96815b10f55e46f1213754e
Bogdan Rogoz <bogdanius96@gmail.com>
2023-05-09 16:17:01 +0300

    configured subnets

--- terraform-kube/main.tf
+++ terraform-kube/main.tf
@@ -22,8 +22,9 @@ resource "aws_vpc" "kube-vpc-01" {
 }
 
 resource "aws_subnet" "kube-subnet-01" {
-  vpc_id            = aws_vpc.kube-vpc-01.id
-  cidr_block        = "172.16.10.0/28"
+  vpc_id                  = aws_vpc.kube-vpc-01.id
+  cidr_block              = "172.16.50.0/28"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "kube-subnet-01"
@@ -35,6 +36,7 @@ resource "aws_instance" "kube-master-01" {
   ami           = "ami-064087b8d355e9051"
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.kube-subnet-01.id
+  key_name      = aws_key_pair.kube-ssh-keypair-01.key_name
 
   tags = {
     Name = "kube-master-01"
--- terraform-kube/workers.tf
+++ terraform-kube/workers.tf
@@ -1,6 +1,8 @@
 resource "aws_instance" "kube-worker-01" {
   ami           = "ami-064087b8d355e9051"
   instance_type = "t3.micro"
+  subnet_id     = aws_subnet.kube-subnet-01.id
+  key_name      = aws_key_pair.kube-ssh-keypair-01.key_name
 
   tags = {
     Name = "kube-worker-01"
@@ -11,6 +13,8 @@ resource "aws_instance" "kube-worker-01" {
 resource "aws_instance" "kube-worker-02" {
   ami           = "ami-064087b8d355e9051"
   instance_type = "t3.micro"
+  subnet_id     = aws_subnet.kube-subnet-01.id
+  key_name      = aws_key_pair.kube-ssh-keypair-01.key_name
 
   tags = {
     Name = "kube-worker-02"
