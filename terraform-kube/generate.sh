6f0923ca1d8b97f6999ea895e9130e2d34c9d926
Bogdan Rogoz <bogdanius96@gmail.com>
2023-05-20 20:34:20 +0300

    added ansible hosts file generator

---
+++ terraform-kube/generate.sh
@@ -0,0 +1,6 @@
+MASTER_IP=$(terraform output -raw kube-master-01-ip)
+WORKER01_IP=$(terraform output -raw kube-worker-01-ip)
+WORKER02_IP=$(terraform output -raw kube-worker-02-ip)
+
+cat hosts-template | sed "s/MASTERIP/$MASTER_IP/" | sed "s/WORKER1IP/$WORKER01_IP/" | sed "s/WORKER2IP/$WORKER02_IP/" > ../ansible-kube/hosts
+
