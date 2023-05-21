1868679a99dd7e4c1a3054d316a614b9346f96ef
Bogdan Rogoz <bogdanius96@gmail.com>
2023-05-21 05:46:26 +0300

    added script for ansible playbook commands

---
+++ ansible-kube/generate.sh
@@ -0,0 +1,4 @@
+ansible-playbook -i hosts install-k8s.yaml --key-file ../ssh-keys/aws
+ansible-playbook -i hosts config-master.yaml --key-file ../ssh-keys/aws
+ansible-playbook -i hosts config-worker.yaml --key-file ../ssh-keys/aws
+
