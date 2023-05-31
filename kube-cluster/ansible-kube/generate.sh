dcafafb8bca9b2c245bf826d22d6983a4ba206b0
Bogdan Rogoz <bogdanius96@gmail.com>
2023-06-01 01:20:34 +0300

    automatic fix commit

--- kube-cluster/ansible-kube/generate.sh
+++ kube-cluster/ansible-kube/generate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ansible-playbook -i hosts install-k8s.yaml --key-file ../ssh-keys/aws
-ansible-playbook -i hosts config-master.yaml --key-file ../ssh-keys/aws
-ansible-playbook -i hosts config-worker.yaml --key-file ../ssh-keys/aws
-ansible-playbook -i hosts install-misc.yaml --key-file ../ssh-keys/aws
+ansible-playbook -i hosts install-k8s.yaml --key-file ../../ssh-keys/aws
+ansible-playbook -i hosts config-master.yaml --key-file ../../ssh-keys/aws
+ansible-playbook -i hosts config-worker.yaml --key-file ../../ssh-keys/aws
+ansible-playbook -i hosts install-misc.yaml --key-file ../../ssh-keys/aws
 
