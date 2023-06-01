4fc6b4e8413254d69d5b6bf9ae518fdabb74c493
Bogdan Rogoz <bogdanius96@gmail.com>
2023-06-01 03:37:11 +0300

    automatic fix commit

--- kube-cluster/ansible-kube/generate.sh
+++ kube-cluster/ansible-kube/generate.sh
@@ -10,5 +10,5 @@ popd
 ansible-playbook -i hosts install-k8s.yaml --key-file ../../ssh-keys/aws
 ansible-playbook -i hosts config-master.yaml --key-file ../../ssh-keys/aws
 ansible-playbook -i hosts config-worker.yaml --key-file ../../ssh-keys/aws
-ansible-playbook -i hosts install-misc.yaml --key-file ../../ssh-keys/aws --extra-vars "prometheus_volume_id=$PROMETHEUS_VOLID grafana_volume_id=$GRAFANA_VOLID"
+ansible-playbook -i hosts install-misc.yaml --key-file ../../ssh-keys/aws --extra-vars "prometheus_volume_id=$PROMETHEUS_VOLID grafana_volume_id=$GRAFANA_VOLID aws_key_id=$AWS_ACCESS_KEY_ID aws_secret_key=$AWS_SECRET_ACCESS_KEY"
 
